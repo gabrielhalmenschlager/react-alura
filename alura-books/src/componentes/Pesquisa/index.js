c5aa217794c2399ec2521210e89302a2dec4cdd0
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-08-10 20:57:56 -0300

    Componenstes dinâmicos

--- alura-books/src/componentes/Pesquisa/index.js
+++ alura-books/src/componentes/Pesquisa/index.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
     color: #FFF;
     text-align: center;
     padding: 85px 0;
@@ -24,23 +25,44 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
-function Pesquisa() {
-    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 
-    console.log(livrosPesquisados)
+function Pesquisa() {
+    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
 
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input
-                placeholder='Ecreva sua próxima leitura'
+                placeholder="Escreva sua próxima leitura"
                 onBlur={evento => {
                     const textoDigitado = evento.target.value
-                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado) )
                     setLivrosPesquisados(resultadoPesquisa)
                 }}
             />
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <img src={livro.src}/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            )) }
         </PesquisaContainer>
     )
 }
--- alura-books/src/index.js
+++ alura-books/src/index.js
@@ -12,6 +12,7 @@ const GlobalStyle = createGlobalStyle`
       sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+      background-image: linear-gradient(90deg, #002F52 35%, #326589);
   }
 
   code {
