4937db329a02dc404f7468965303af312af5334f
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-08-09 22:36:29 -0300

    Começando o primeiro componente

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import logo from './imagens/logo.svg'
 
 function App() {
   return (
-    <div>
-      <header>
-
+    <div className='App'>
+      <header className='App-header'>
+      <div className='logo'>
+        <img src={logo} alt='logo'></img>
+        <p><strong>Alura</strong>Books</p>
+      </div>
       </header>
     </div>
   );
