e001674e233ffe1ae348fc0a84eb213573cb87e8
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-08-09 23:45:10 -0300

    Criando o componente Header e simplificando o código

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Logo from './componentes/Logo' 
-import perfil from './imagens/perfil.svg'
-import sacola from './imagens/sacola.svg'
-import OpcoesHeader from './componentes/OpcoesHeader'
+import Header from './componentes/Header';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-
-      </header>
+      <Header/>
     </div>
   );
 }
--- alura-books/src/componentes/IconesHeader/index.js
+++ alura-books/src/componentes/IconesHeader/index.js
@@ -1,4 +1,6 @@
 import './estilo.css'
+import perfil from '../../imagens/perfil.svg'
+import sacola from '../../imagens/sacola.svg'
 
 const icones = [perfil, sacola] 
 
