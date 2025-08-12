bf7522b30b4f3bac3cb85623f4e4fa8a22c45fdc
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-08-12 11:22:50 -0300

    Começando o get de dados em arquivos

--- alura-books-server/teste.js
+++ alura-books-server/teste.js
@@ -1,8 +1,8 @@
-const fs = require("fs")
+// const fs = require("fs")
 
-const dadosAtuais = JSON.parse(fs.readFileSync("livros.json"))
-const novoDado = { id: "9", nome: "A saga do Node.js" }
+// const dadosAtuais = JSON.parse(fs.readFileSync("livros.json"))
+// const novoDado = { id: "9", nome: "A saga do Node.js" }
 
-fs.writeFileSync("livros.json", JSON.stringify([...dadosAtuais, novoDado]))
+// fs.writeFileSync("livros.json", JSON.stringify([...dadosAtuais, novoDado]))
 
-console.log(JSON.parse(fs.readFileSync("livros.json")))
+// console.log(JSON.parse(fs.readFileSync("livros.json")))
