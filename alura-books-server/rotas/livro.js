194d2d08ba1c3c0acde0854e611ce3b8bbcb08b8
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-08-12 09:44:30 -0300

    Status HTTP

--- alura-books-server/rotas/livro.js
+++ alura-books-server/rotas/livro.js
@@ -3,7 +3,12 @@ const { Router } = require("express")
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.send("Você fez uma requisição do tipo GET")
+    try {
+        res.send("Você fez uma requisição do tipo GET")
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
 } ) 
 
 router.post("/", (req, res) => {
