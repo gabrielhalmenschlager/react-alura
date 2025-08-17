31daf4f1bda7d0dccefcf566b8c3be5bea14604c
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-08-17 20:05:18 -0300

    Post a partir do body

--- alura-books-server/rotas/livro.js
+++ alura-books-server/rotas/livro.js
@@ -1,6 +1,5 @@
 const { Router } = require("express")
-const { getLivros } = require("../controladores/livro")
-const { getLivro } = require("../controladores/livro")
+const { getLivros, getLivro, postLivro } = require("../controladores/livro")
 
 const router = Router()
 
@@ -8,9 +7,7 @@ router.get("/", getLivros)
 
 router.get("/:id", getLivro) 
 
-router.post("/", (req, res) => {
-    res.send("Você fez uma requisição do tipo POST")
-} )
+router.post("/", postLivro)
 
 router.patch("/", (req, res) => {
     res.send("Você fez uma requisição do tipo PATCH")
