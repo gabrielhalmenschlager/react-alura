475b533669608a17b60e1579fbcbcccebc19a6d2
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-09-07 13:32:39 -0300

    Alterando o nome do App.js para Home.js

--- alura-books/src/index.js
+++ alura-books/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './componentes/Header'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -30,8 +31,9 @@ root.render(
   <React.StrictMode>
     <GlobalStyle />
     <BrowserRouter>
+    <Header />
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/favoritos" element={<p>favoritos</p>} />
         <Route path="/categorias" element={<p>categorias</p>} />
         <Route path="/estante" element={<p>estante</p>} />
