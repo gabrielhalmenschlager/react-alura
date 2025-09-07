7eaf02f28fafc10b779961ce77e9e972336b2e04
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-09-07 13:34:33 -0300

    Criando a pasta rotas e movendo o Home.js para dentro dela

--- alura-books/src/index.js
+++ alura-books/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './Home';
+import Home from './rotas/Home';
 import reportWebVitals from './reportWebVitals';
 import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
