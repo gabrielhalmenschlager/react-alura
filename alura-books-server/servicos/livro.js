73d2e2598517f6311fd8667d254bdf53f78f5db4
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-09-08 10:57:23 -0300

    Serviço de livros favoritos

--- alura-books-server/servicos/livro.js
+++ alura-books-server/servicos/livro.js
@@ -15,23 +15,22 @@ function insereLivro(livroNovo) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
     const novaListaDeLivros = [ ...livros, livroNovo]
-
     fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros))
 }
 
 function modificaLivro(modificacoes,  id) {
     let livrosAtuais = JSON.parse(fs.readFileSync("livros.json"))
-    const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
-    
-    const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
 
+    const indiceModificado = livrosAtuais.findIndex(livro => livro.id === id)
+    const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
+    
     livrosAtuais[indiceModificado] = conteudoMudado
     fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
 }
 
 function deletarLivroPorId(id) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
-
+    
     const livrosFiltrados = livros.filter( livro => livro.id !== id )
     fs.writeFileSync("livros.json", JSON.stringfy(livrosFiltrados))
 }
